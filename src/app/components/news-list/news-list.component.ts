2502fdcbd100482dc32aa4714a9784e069c3b16e
beibarys.zholmyrza <beibarys.zholmyrza@btsdigital.kz>
2023-02-16 17:32:38 +0600

    fix pagination total number of records

--- src/app/components/news-list/news-list.component.ts
+++ src/app/components/news-list/news-list.component.ts
@@ -1,8 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
-import { NewsService } from "../../services/newsService";
-import {News, PageInfo, Res} from "../../models/model";
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {NewsService} from "../../services/newsService";
+import {News, PageInfo} from "../../models/model";
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -10,18 +9,25 @@ import { Router } from '@angular/router';
   templateUrl: './news-list.component.html',
   styleUrls: ['./news-list.component.css']
 })
-export class NewsListComponent implements OnInit {
+export class NewsListComponent implements OnChanges {
   @Input() news!: News[]
   @Input() page_info!: PageInfo
+  totalItems!: number;
 
 
   constructor(
-    private newsService: NewsService, 
+    private newsService: NewsService,
     private router: Router
     ) { }
 
   ngOnInit(): void {
   }
 
-
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.page_info) {
+      this.totalItems = this.page_info.total_pages * 15;
+    } else {
+      this.totalItems = 0;
+    }
+    }
 }
